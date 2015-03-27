f1f1c5819b7edae0cd694d092a0a36e00ca55926
Jos Kuijpers <jos@kuijpersvof.nl>
2015-03-27 15:52:00 +0100

    Some formatting.

--- src/io/github/joskuijpers/datamining_challenge/tiers/StatData.java
+++ src/io/github/joskuijpers/datamining_challenge/tiers/StatData.java
@@ -6,53 +6,48 @@ import io.github.joskuijpers.datamining_challenge.model.User;
 
 /**
  * Een klasse om wat data uit de gegevens te halen.
+ * 
  * @author Jan
- *
  */
-public class StatData extends Tier{
+public class StatData extends Tier {
 	
-	public static TierData run(TierData data){
+	public static TierData run(TierData data) {
+		// uitrekenen gegevens userlist.
+		int aantalUsers = 0;
+		int aantalMannen = 0;
+		int somLeeftijd = 0;
+		int somLeeftijdMannen = 0;
+		int somNumberRatings = 0;
 		
-		//uitrekenen gegevens userlist.
-		int aantalUsers=0;
-		int aantalMannen=0;
-		int somLeeftijd=0;
-		int somLeeftijdMannen=0;
-		int somNumberRatings=0;
-		
-		
-				
 		for (User user : data.getUserList()) {
 			aantalUsers++;
-			if(user.isMale()){
+			if (user.isMale()) {
 				aantalMannen++;
-				somLeeftijdMannen+=user.getAge();
+				somLeeftijdMannen += user.getAge();
 			}
-			somLeeftijd+=user.getAge();
-			somNumberRatings+=user.getNumberOfRatings();
+			somLeeftijd += user.getAge();
+			somNumberRatings += user.getNumberOfRatings();
 		}
-		float gemLeeftijd= (float)somLeeftijd/(float)aantalUsers;
-		float gemLeeftijdMannen= (float)somLeeftijdMannen/(float)aantalMannen;
-		float gemAantalRatings= (float)somNumberRatings/(float)aantalUsers;
 		
-		System.out.println("Aantal users is: "+aantalUsers);
-		System.out.println("Daarvan zijn mannen: "+aantalMannen);
-		System.out.println("Daarvan zijn vrouwen: "+ (aantalUsers-aantalMannen));
-		System.out.println("De gemiddelde leeftijd is: "+gemLeeftijd);
-		System.out.println("De gemiddelde leeftijd van de mannen is: "+gemLeeftijdMannen);
-		System.out.println("De gemiddelde aantal ratings is: "+ gemAantalRatings);
+		float gemLeeftijd = (float) somLeeftijd / (float) aantalUsers;
+		float gemLeeftijdMannen = (float) somLeeftijdMannen / (float) aantalMannen;
+		float gemAantalRatings = (float) somNumberRatings / (float) aantalUsers;
 		
-		int totalmovies= 0;
-		int sumNumberofRatings =0;
+		System.out.println("Aantal users is: " + aantalUsers);
+		System.out.println("Daarvan zijn mannen: " + aantalMannen);
+		System.out.println("Daarvan zijn vrouwen: " + (aantalUsers - aantalMannen));
+		System.out.println("De gemiddelde leeftijd is: " + gemLeeftijd);
+		System.out.println("De gemiddelde leeftijd van de mannen is: " + gemLeeftijdMannen);
+		System.out.println("De gemiddelde aantal ratings is: " + gemAantalRatings);
+		
+		int totalmovies = 0;
+		int sumNumberofRatings = 0;
 		for (Movie movie : data.getMovieList()) {
 			totalmovies++;
-			sumNumberofRatings+=movie.getNumberOfRatings();
+			sumNumberofRatings += movie.getNumberOfRatings();
 		}
-		float genNumRat= (float)sumNumberofRatings/(float)totalmovies;
+		float genNumRat = (float) sumNumberofRatings / (float) totalmovies;
 		System.out.println(genNumRat);
 		return data;
 	}
-	
-	
-
 }
